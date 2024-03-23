90236d22c53be180b82831ac00b6a5c424dd1563
rmalegr <rodrigomalegre@gmail.com>
2024-03-23 13:13:16 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Empleado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Descripcion  { get; set;}
+        public double Sueldo { get; set; }
+        public  int Edad { get; set; }
+
+        public Empleado(string nombre, string descripcion, double sueldo, int edad)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Sueldo = sueldo;
+            Edad = edad;
+        }
+
+        
+
+       
+
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x;// declaration 
+        x = 123;
+        int y = 321; //declaration + initialization
+
+        int z = x + y;
+        int age = 40;
+        double height = 300.5;
+        bool alive = true;
+        char symbol = '@';
+
+        Console.WriteLine("The result of x + y is =  " + z);
+        Console.WriteLine("Your age is " + age);
+        Console.WriteLine("Your height  is " + height + " cm" );
+        Console.WriteLine("Are you alive ?  " + alive);
+        Console.WriteLine("Your symbol prefer " + symbol);
+
+        Console.ReadKey();
+
+
+    }
+}
